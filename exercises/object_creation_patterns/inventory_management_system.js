1e4d55323451354cad6aff584024137ae266b6cf
Fernanda Franco <francodias.fernanda@gmail.com>
2018-09-11 19:33:39 -0400

    exercises: almmost done with obbj creation patterns

--- exercises/object_creation_patterns/inventory_management_system.js
+++ exercises/object_creation_patterns/inventory_management_system.js
@@ -1,33 +1,134 @@
-function Item(itemName, category, quantity) {
-  this.itemName = itemName;
-  this.category = category;
-  this.quantity = quantity;
-  this.skuCode = this.itemName.replace(' ', '').slice(0, 3) + this.category.slice(0, 2);
-}
+var ItemCreator = (function() {
+  function generateSkuCode(itemName, category) {
+    return (itemName.replace(/\s/g, '').slice(0, 3).toUpperCase() +
+            category.replace(/\s/g, '').slice(0, 2).toUpperCase());
+  }
 
-var itemManager = {
+  function isValidItemName(itemName) {
+    return itemName.replace(/\s/g, '').length >= 5;
+  }
+
+  function isValidCategory(category) {
+    return category.replace(/\s/g, '').length >= 5 && category.split(' ').length === 1;
+  }
+
+  function isQuantityProvided(quantity) {
+    return quantity !== undefined;
+  }
+
+  return function(itemName, category, quantity) {
+    if (isValidItemName(itemName) && isValidCategory(category) && isQuantityProvided(quantity)) {
+      this.skuCode = generateSkuCode(itemName, category);
+      this.itemName = itemName;
+      this.category = category;
+      this.quantity = quantity;
+    } else {
+      return { notValid: true };
+    }
+  };
+})();
+
+var ItemManager = {
   items: [],
+  getItem: function(skuCode) {
+    return this.items.filter(function(item) {
+      return item.skuCode === skuCode;
+    })[0];
+  },
+
   create: function(itemName, category, quantity) {
-    var newItem = Item(itemName, category, quantity);
-    items.push(newItem);
-    return newItem;
-  },
-  update: function(skuCode, updatedInfo) {
-    var i;
-
-    for (i = 0; i < items.length; i += 1) {
-      if (items[i].skuCode === skucode) {
-        items[i];
-      }
+    var item = new ItemCreator(itemName, category, quantity);
+    if (item.notValid) {
+      return false;
+    } else {
+      this.items.push(item);
     }
   },
+
+  update: function(skuCode, itemInformation) {
+    Object.assign(this.getItem(skuCode), itemInformation);
+  },
+
   delete: function(skuCode) {
-    var i;
-
-    for (i = 0; i < items.length; i += 1) {
-      if (items[i].skuCode === skucode) {
-        items.splice(i, 1);
-      }
-    }
+    this.items.splice(this.items.indexOf(this.getItem(skuCode)), 1);
   },
-}
+
+  inStock: function() {
+    return this.items.filter(function(item) {
+      return item.quantity > 0;
+    });
+  },
+  itemsInCategory: function(category) {
+    return this.items.filter(function(item) {
+      return item.category === category;
+    });
+  },
+};
+
+var ReportManager = {
+  init: function(itemManager) {
+    this.items = itemManager;
+  },
+
+  createReporter: function(skuCode) {
+    var that = this;
+    return {
+      itemInfo: function() {
+        var item = that.items.getItem(skuCode);
+
+        Object.keys(item).forEach(function(property) {
+          console.log(property + ': ' + item[property]);
+        });
+      },
+    };
+  },
+  reportInStock: function() {
+    console.log(this.items.inStock().map(function(item) {
+      return item.itemName;
+    }))
+  }
+};
+
+ItemManager.create('basket ball', 'sports', 0);           // valid item
+ItemManager.create('asd', 'sports', 0);
+ItemManager.create('soccer ball', 'sports', 5);           // valid item
+ItemManager.create('football', 'sports');
+ItemManager.create('football', 'sports', 3);              // valid item
+ItemManager.create('kitchen pot', 'cooking items', 0);
+ItemManager.create('kitchen pot', 'cooking', 3);          // valid item
+// returns list with the 4 valid items
+console.log(ItemManager.items);
+
+ReportManager.init(ItemManager);
+// logs soccer ball,football,kitchen pot
+ReportManager.reportInStock();
+
+ItemManager.update('SOCSP', { quantity: 0 });
+// returns list with the item objects for football and kitchen pot
+console.log(ItemManager.inStock());
+// football,kitchen pot
+ReportManager.reportInStock();
+
+// returns list with the item objects for basket ball, soccer ball, and football
+ItemManager.itemsInCategory('sports');
+
+ItemManager.delete('SOCSP');
+// returns list the the remaining 3 valid items (soccer ball is removed from the list)
+ItemManager.items;
+
+var kitchenPotReporter = ReportManager.createReporter('KITCO');
+kitchenPotReporter.itemInfo();
+// logs
+// skuCode: KITCO
+// itemName: kitchen pot
+// category: cooking
+// quantity: 3
+
+ItemManager.update('KITCO', { quantity: 10 });
+kitchenPotReporter.itemInfo();
+// logs
+// skuCode: KITCO
+// itemName: kitchen pot
+// category: cooking
+// quantity: 10
+co
